a825ef7f24ee11d4bcc62f7ff686ed9b8684cc2d
Jakub Cieslik <jm.cieslik@gmail.com>
2015-04-25 15:16:54 +0200

    Buttons

--- KinectKeyboard/MainWindow.xaml.cs
+++ KinectKeyboard/MainWindow.xaml.cs
@@ -30,6 +30,34 @@ namespace KinectKeyboard
 
             _qwertyButtons = new ObservableCollection<Components.Button>();
 
+            _qwertyButtons.Add(new Components.Button("Q", 0, 0));
+            _qwertyButtons.Add(new Components.Button("W", 0, 1));
+            _qwertyButtons.Add(new Components.Button("E", 0, 2));
+            _qwertyButtons.Add(new Components.Button("R", 0, 3));
+            _qwertyButtons.Add(new Components.Button("T", 0, 4));
+            _qwertyButtons.Add(new Components.Button("Y", 0, 5));
+            _qwertyButtons.Add(new Components.Button("U", 0, 6));
+            _qwertyButtons.Add(new Components.Button("I", 0, 7));
+            _qwertyButtons.Add(new Components.Button("O", 0, 8));
+            _qwertyButtons.Add(new Components.Button("P", 0, 9));
+
+            _qwertyButtons.Add(new Components.Button("A", 1, 0));
+            _qwertyButtons.Add(new Components.Button("S", 1, 1));
+            _qwertyButtons.Add(new Components.Button("D", 1, 2));
+            _qwertyButtons.Add(new Components.Button("F", 1, 3));
+            _qwertyButtons.Add(new Components.Button("G", 1, 4));
+            _qwertyButtons.Add(new Components.Button("H", 1, 5));
+            _qwertyButtons.Add(new Components.Button("J", 1, 6));
+            _qwertyButtons.Add(new Components.Button("K", 1, 7));
+            _qwertyButtons.Add(new Components.Button("L", 1, 8));
+
+            _qwertyButtons.Add(new Components.Button("Z", 2, 0));
+            _qwertyButtons.Add(new Components.Button("X", 2, 1));
+            _qwertyButtons.Add(new Components.Button("C", 2, 2));
+            _qwertyButtons.Add(new Components.Button("V", 2, 3));
+            _qwertyButtons.Add(new Components.Button("B", 2, 4));
+            _qwertyButtons.Add(new Components.Button("N", 2, 5));
+            _qwertyButtons.Add(new Components.Button("M", 2, 6));
             //_qwertyButtons.Add(new Components.Button(Key, 1, 3));
             //for (int i = 44; i < 70; i++)
             //{
